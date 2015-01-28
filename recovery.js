bf4c1109815a72d3696392773235fee5f447bb34
deanoemcke <deanoemcke@gmail.com>
2015-01-29 08:08:25 +1300

    Splash page style tweaks

--- recovery.js
+++ recovery.js
@@ -10,14 +10,19 @@
             window.clearInterval(readyStateCheckInterval);
 
             var restoreEl = document.getElementById('restoreSession'),
-                cancelEl = document.getElementById('closeTab');
+                cancelEl = document.getElementById('closeTab'),
+                manageEl = document.getElementById('manageManuallyLink');
 
             restoreEl.onclick = function (e) {
+                debugger;
                 gsUtils.recoverLostTabs();
                 chrome.tabs.getCurrent(function(t) {chrome.tabs.remove(t.id);});
             };
             cancelEl.onclick = function (e) {
                 chrome.tabs.getCurrent(function(t) {chrome.tabs.remove(t.id);});
+            };
+            manageEl.onclick = function (e) {
+                chrome.tabs.create({url: chrome.extension.getURL('history.html')});
             };
         }
     }, 50);
