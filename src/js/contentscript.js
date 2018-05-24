ca8f674ae6bf3c5c91a6e01f8c33e18922f1ab39
Unknown <shanedrgn@gmail.com>
2018-05-23 19:34:26 -0500

    Made recommended changes
    Made the youtube time-stamp feature no longer optional and made it keep youtube URL parameters on suspend

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -8,6 +8,7 @@
 */
 (function () {
     'use strict';
+    var YOUTUBE_ID_REGEX = /https?:\/\/(?:www\.)?youtube.com\/watch.+?(?:v\=)([0-9A-Za-z_-]{11})/
 
     var isInitialised = false,
         isFormListenerInitialised = false,
@@ -15,7 +16,8 @@
         isIgnoreForms = false,
         tempWhitelist = false,
         timerJob,
-        suspendDateTime = false;
+        suspendDateTime = false,
+        isYoutubeVideo = false;
 
     function suspendTab(suspendedUrl) {
         window.location.replace(suspendedUrl);
@@ -63,6 +65,16 @@
 
         if (request.hasOwnProperty('action') && request.action === 'initialiseContentScript') {
             isInitialised = true;
+            
+            let ytURL = YOUTUBE_ID_REGEX.exec(window.location.href);
+
+
+            if (ytURL) {
+                isYoutubeVideo = true;
+                sendResponse({
+                    action: 'setYoutubeFlag'
+                })
+            }
         }
 
         if (request.hasOwnProperty('scrollPos')) {
@@ -100,12 +112,20 @@
     });
 
     function buildReportTabStatePayload() {
-        return {
+        var state =  {
             action: 'reportTabState',
             isInitialised: isInitialised,
             status: (isIgnoreForms && isReceivingFormInput) ? 'formInput' : (tempWhitelist ? 'tempWhitelist' : 'normal'),
             scrollPos: document.body.scrollTop || document.documentElement.scrollTop || 0,
             timerUp: suspendDateTime ? suspendDateTime + '' : false,
         };
+        if (isYoutubeVideo) {
+            state.timestamp = getYoutubeTimestamp();
+        }
+        return state;
+    }
+
+    function getYoutubeTimestamp() {
+        return document.querySelector('video.video-stream.html5-main-video').currentTime>>0;
     }
 }());
