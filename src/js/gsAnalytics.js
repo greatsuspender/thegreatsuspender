8c7136208ca7ab38563eadaf61d208b1fd10548e
deanoemcke <deanoemcke@gmail.com>
2018-08-27 16:33:00 -0400

    Remove unnessesary gsAnalytics code

--- src/js/gsAnalytics.js
+++ src/js/gsAnalytics.js
@@ -28,12 +28,7 @@ var gsAnalytics = (function() {
       dimension3: gsStorage.getOption(gsStorage.SUSPEND_TIME) + '',
       dimension4: gsStorage.getOption(gsStorage.NO_NAG) + '',
     });
-    ga('send', 'pageview', {
-      dimension1: chrome.runtime.getManifest().version + '',
-      dimension2: gsStorage.getOption(gsStorage.SCREEN_CAPTURE) + '',
-      dimension3: gsStorage.getOption(gsStorage.SUSPEND_TIME) + '',
-      dimension4: gsStorage.getOption(gsStorage.NO_NAG) + '',
-    });
+    ga('send', 'pageview');
   }
 
   return {
