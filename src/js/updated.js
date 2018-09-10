17120082c0e03d87248439d1d13998fca6be9935
deanoemcke <deanoemcke@gmail.com>
2018-09-10 15:50:08 -0400

    Remove reference to restore point from updated.html

--- src/js/updated.js
+++ src/js/updated.js
@@ -2,7 +2,6 @@
 (function() {
   'use strict';
 
-  var gsIndexedDb = chrome.extension.getBackgroundPage().gsIndexedDb;
   var gsSession = chrome.extension.getBackgroundPage().gsSession;
   var gsUtils = chrome.extension.getBackgroundPage().gsUtils;
 
@@ -15,23 +14,7 @@
       chrome.tabs.create({ url: chrome.extension.getURL('history.html') });
     };
 
-    var currentVersion = chrome.runtime.getManifest().version;
     var updateType = gsSession.getUpdateType();
-    gsIndexedDb
-      .fetchSessionRestorePoint(
-        gsIndexedDb.DB_SESSION_POST_UPGRADE_KEY,
-        currentVersion
-      )
-      .then(function(sessionRestorePoint) {
-        if (!sessionRestorePoint) {
-          gsUtils.log(
-            'updated',
-            'Couldnt find session restore point. Something has gone horribly wrong!!'
-          );
-        } else {
-          document.getElementById('backupInfo').style.display = 'block';
-        }
-      });
     if (updateType === 'major' || updateType === 'minor') {
       document.getElementById('patchMessage').style.display = 'none';
     } else {
