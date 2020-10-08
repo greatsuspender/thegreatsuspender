83397e841187bd60db4965f117534ffe67bc70e1
thegreatsuspender <contact@thegreatsuspender.com>
2020-10-08 14:56:15 +0200

    promoted expression to switch-case and added signal for loading blocklist

--- src/js/background.js
+++ src/js/background.js
@@ -1610,37 +1610,37 @@ var tgs = (function() {
       request.action
     );
 
-    if (request.action === 'reportTabState') {
-      var contentScriptStatus =
-        request && request.status ? request.status : null;
-      if (
-        contentScriptStatus === 'formInput' ||
-        contentScriptStatus === 'tempWhitelist'
-      ) {
-        chrome.tabs.update(sender.tab.id, { autoDiscardable: false });
-      } else if (!sender.tab.autoDiscardable) {
-        chrome.tabs.update(sender.tab.id, { autoDiscardable: true });
-      }
-      // If tab is currently visible then update popup icon
-      if (sender.tab && isCurrentFocusedTab(sender.tab)) {
-        calculateTabStatus(sender.tab, contentScriptStatus, function(status) {
-          setIconStatus(status, sender.tab.id);
-        });
-      }
-      sendResponse();
-      return false;
+    switch (request.action) {
+      case 'loadCleanScreencaptureBlocklist':
+        gsCleanScreencaps.loadList()
+      case 'reportTabState':
+        var contentScriptStatus =
+          request && request.status ? request.status : null;
+        if (
+          contentScriptStatus === 'formInput' ||
+          contentScriptStatus === 'tempWhitelist'
+        ) {
+          chrome.tabs.update(sender.tab.id, { autoDiscardable: false });
+        } else if (!sender.tab.autoDiscardable) {
+          chrome.tabs.update(sender.tab.id, { autoDiscardable: true });
+        }
+        // If tab is currently visible then update popup icon
+        if (sender.tab && isCurrentFocusedTab(sender.tab)) {
+          calculateTabStatus(sender.tab, contentScriptStatus, function (status) {
+            setIconStatus(status, sender.tab.id);
+          });
+        }
+        sendResponse();
+        return false;
+      case 'savePreviewData':
+        gsTabSuspendManager.handlePreviewImageResponse(
+          sender.tab,
+          request.previewUrl,
+          request.errorMsg
+        ); // async. unhandled promise
+        sendResponse();
+        return false;
     }
-
-    if (request.action === 'savePreviewData') {
-      gsTabSuspendManager.handlePreviewImageResponse(
-        sender.tab,
-        request.previewUrl,
-        request.errorMsg
-      ); // async. unhandled promise
-      sendResponse();
-      return false;
-    }
-
     // Fallback to empty response to ensure callback is made
     sendResponse();
     return false;
